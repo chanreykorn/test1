238f030ee49355b72814f2a20c199ce33f2d3e67
chanrey korn <kornchanrey513@gmail.com>
2023-07-11 19:07:02 +0700

    test2

---
+++ JavaScript.js
@@ -0,0 +1,8 @@
+let image = document.querySelector("img");
+let input = document.querySelector("input");
+input.addEventListener("change", () => {
+    image.src = URL.createObjectURL(input.files[0]);
+    
+});
+
+
